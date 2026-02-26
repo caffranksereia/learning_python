085bac5868dac428118626d0de9c3765518d831f
mr nobody <circuncisaofabio@icloud.com>
2026-02-26 13:10:56 -0300

    Learning  string

---
+++ learning_3.py
@@ -0,0 +1,27 @@
+"""
+Python = program language
+dynamic and strong typing
+str -> string -> text
+
+strings are text
+
+"""
+
+#sigle quotation marks
+print('Mr nobody')
+
+#duple quotation marks
+print("Mr nobody")
+#scape \ interpreter ignore 
+print("Mr \"nobody\"")
+
+# r  regular expressions
+print(r"Mr \"nobody\"")
+
+#advanced 
+#use sigle quotation marks for print "" 
+print(1,'"Mr \"nobody\"')
+
+#use duple quotation marks for print ''
+print(2,"Mr nobody'\'")
+
