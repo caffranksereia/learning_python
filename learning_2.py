5b6be5041e5876aab19b1d40d587bc761839f21b
mr nobody <circuncisaofabio@icloud.com>
2026-02-26 13:10:41 -0300

    learning 2 about print and ard

---
+++ learning_2.py
@@ -0,0 +1,11 @@
+#here you will learn about  the print() funct
+
+print('Hello World!')
+
+#argm sep is a portugues  separador it's an unnomed
+#line broke \r  return or \n line feed - CRLF windows
+# \n ->LF
+print(10,20,30, sep="bleep", end='\r\n')
+#end sign final print afte ult
+print(40,50,60, sep='bloop',end='##\n')
+print(40,50,60, sep='bloop',end='\n\n')
