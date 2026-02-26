0eb3d0dc2e7b79d6761056ca67d9eae39657fccf
mr nobody <circuncisaofabio@icloud.com>
2026-02-26 13:10:21 -0300

    Learning 1 cap about comment

---
+++ learning_1.py
@@ -0,0 +1,6 @@
+#comentario para linha unica
+#comentario '''comentario linhas multiplas o''''' ou """ DOCString""" 
+'''comentario linhas multiplas o'''
+#ou 
+""" DOCString""" 
+print('Hello World!')
