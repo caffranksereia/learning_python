b384f88cbc22dd95b5bd3161e6bf8e08048b014d
mr nobody <circuncisaofabio@icloud.com>
2026-02-26 13:11:08 -0300

    learning float and int

---
+++ learning_4.py
@@ -0,0 +1,14 @@
+""" 
+    Types int and float
+    int -> integer
+    The int type allows positive or negative numbers
+    Positive int  sign
+
+    float -> floating-point number
+    The float type allows positive or negative numbers
+
+    Positive float sign
+
+    The type function shows the type that Python
+
+"""
