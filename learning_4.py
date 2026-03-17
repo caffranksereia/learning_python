0aae631b9a35fa37c1bb55f086350ade18847562
mr nobody <circuncisaofabio@icloud.com>
2026-03-17 11:11:29 -0300

    translate portuguese to english comment

--- learning_4.py
+++ learning_4.py
@@ -11,4 +11,24 @@
 
     The type function shows the type that Python
 
+    everthing lowercase and exect call calabo its class
+
 """
+
+#int
+int_a = -11
+int_b = 0
+int_c = 11
+
+print(int_a, int_b,int_c )
+
+#float
+float_a = -11.5
+float_b = 0.05
+float_c = 11.6
+print(float_a, float_b,float_c )
+
+
+#The type function shows the type that Python but this's class
+
+print(type(float_c))
