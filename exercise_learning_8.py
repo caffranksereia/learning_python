1748a05b8f649010828b1da81ff2c49506fc9325
mr nobody <circuncisaofabio@icloud.com>
2026-03-17 11:13:57 -0300

    exercise about input for user info

---
+++ exercise_learning_8.py
@@ -0,0 +1,18 @@
+name = input("What's your name:")
+last_name = input("And Last Name:")
+age = int(input("How old are you:"))
+birthday = input("When is your birthday?:")
+of_legal_age = 18
+age_verification = age >= of_legal_age
+height = int(input("what's your height:"))
+number_convt = 100
+height_convt = float( height / number_convt )
+
+
+
+print('Name:',name)
+print('Last Name:', last_name)
+print('Age:',age)
+print('Birthday:', birthday)
+print('Age verification:', age_verification)
+print('height', height_convt, 'height')
