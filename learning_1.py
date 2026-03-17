6e7db49df68656560d5b8060f2d7487dc0e824de
mr nobody <circuncisaofabio@icloud.com>
2026-03-17 11:08:38 -0300

    change portugues to english about comment in python

--- learning_1.py
+++ learning_1.py
@@ -1,6 +1,8 @@
-#comentario para linha unica
-#comentario '''comentario linhas multiplas o''''' ou """ DOCString""" 
-'''comentario linhas multiplas o'''
+# → used for single-line comments
+"""
+    ''' ''' or """ """ → used for multi-line strings (often used as docstrings)
+"""
+
 #ou 
 """ DOCString""" 
 print('Hello World!')
