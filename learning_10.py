6d1df36b44ab339d2afeabfe3e8a4b12bb22c224
mr nobody <circuncisaofabio@icloud.com>
2026-03-17 11:20:36 -0300

    addition operator

---
+++ learning_10.py
@@ -0,0 +1,8 @@
+#the addition operator can be use for concat 
+concat = 'Fabio' + '' + 'Eduardo'
+print(concat)
+#The multiplication sign can be used to repeat things.
+mult_tenfold = 'A' * 10 
+threefold = 3 * '-'
+print(mult_tenfold)
+print(threefold)
