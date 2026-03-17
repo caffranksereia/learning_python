d2bd865c67192fa23b26ba53ba4b1aac04e9cc22
mr nobody <circuncisaofabio@icloud.com>
2026-03-17 11:21:16 -0300

    Previous operation

---
+++ learning_11.py
@@ -0,0 +1,16 @@
+"""
+
+    Previous operation
+ 1 - (n + n)
+ 2 - **
+ 3 - * / // %
+ 4 - + -
+
+"""
+
+cont =  1 + 1 ** 5 + 5 
+# the interpreter changed the restult because it received another value
+
+cont = 'something'
+
+print(cont) 
