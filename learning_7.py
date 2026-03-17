a0a3d90cca51020022a864ba811b6e1d5a51c8de
mr nobody <circuncisaofabio@icloud.com>
2026-03-17 11:18:41 -0300

    store data in variable use lowcase ans snake case (_)

---
+++ learning_7.py
@@ -0,0 +1,27 @@
+"""
+  
+  We can store data in a virable in the  computer
+   - PEP8: Variable names should start with lowercase letters. can use numbers and underscores (_).
+   the =(iguals ) sign  the assignment operator. its used to assign  a value to varible(varible)
+
+   ex: 
+   variable_name = expression
+
+"""
+
+complet_name  = 'Fabio Eduardo'
+add_two_plus_two = 2 + 2
+
+print(complet_name,' snake_case')
+print(complet_name, add_two_plus_two)
+
+int_um = int('1')
+#if can convert to bool
+print(int_um, type(int_um))
+
+user = 'Fabio'
+age = 32 
+verific_age= 32
+nome = user
+confirm_age = verific_age >= age
+print(user, verific_age, verific_age)
