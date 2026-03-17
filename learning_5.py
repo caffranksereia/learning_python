128e7b53f3d60ec9de9fbf31e69002994dfed33e
mr nobody <circuncisaofabio@icloud.com>
2026-03-17 11:15:40 -0300

    data typoe bool logic operator

---
+++ learning_5.py
@@ -0,0 +1,17 @@
+"""
+data type: bool( boolean)
+when yuou ask a question in a program , 
+there are two answers:
+yes(true) or no(false)
+there are several operators 
+one of then is, logic operator questing if the value is iguals to other
+
+
+"""
+
+print(10 == 10)
+print(10 == 11)
+print(type(True))
+print(type(False))
+print( type( 10 == 10 ))
+print(type(10 == 11))
