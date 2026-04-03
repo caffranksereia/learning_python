ff779cee83b214cce8d4afa192c03b8c5bcc2052
mr nobody <circuncisaofabio@icloud.com>
2026-04-02 22:48:35 -0300

    Comparison  Operators operator   Meaning  Example (true)

---
+++ learning_17.py
@@ -0,0 +1,19 @@
+"""
+Comparison  Operators 
+operator   Meaning  Example (true)
+
+>         greater than       2 > 1
+>=       greater than or equal to   2 >= 2
+<            less than        1 < 2
+<=       less than or equal to  2 <= 2
+==           equal      'a' == 'a'
+!=       not equal to  'a' != 'b'
+
+"""
+
+greater = 2 >1 
+greater_than_or_equal_to = 2 >= 2
+less_than = 1 < 2
+less_than_or_equal_to = 2 <= 2
+equal = 'a' == 'a'
+not_equal_to = 'a' != 'b'
