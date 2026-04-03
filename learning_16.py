cbb0ba8ef808c4c441ce48f43c401f4b95137cb0
mr nobody <circuncisaofabio@icloud.com>
2026-04-02 22:46:42 -0300

    explain about condition use if, else and elif

---
+++ learning_16.py
@@ -0,0 +1,14 @@
+#if /elif   / else
+pull =  input('Se quiser sair \"digite sair" se quiser entrar \"digite entrar ')
+pull_user_get_out = "sair" and "Sair"
+pull_user_get_in = "entrar" and "Entrar"
+
+if pull ==  pull_user_get_in: 
+    print("Voce entrou")
+
+elif pull == pull_user_get_out :
+    print("voce saiu")
+
+else:
+    print("nada por aqui!")
+
